6835de1220429d9917671b8040c9c09789f37ed8
rawatsun <suraj.singh@osscube.com>
2013-03-13 02:49:54 -0700

    this will contains the main website index file as a main.php

---
+++ visiondart/view/main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!--https://developers.google.com/speed/libraries/devguide-->
+    <!--
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+     <link rel="stylesheet" href="./css/reset.css" />
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+             
+    -->
+
+    <title>visiondart - index </title>
+    <script src="jquery-lib/jquery-1.8.3.js" type="text/javascript"></script>
+    <script src="./js/JScript.js" type="text/javascript"></script>
+    <link href="./css/reset.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="main-container">
+        <?php include_once('main-websit-eresource/top.php') ?>
+
+
+        <?php include_once('main-websit-eresource/header.php') ?>
+
+
+
+    <div style="clear:both"></div>
+
+    <dl>
+
+        <?php include_once('main-websit-eresource/home.php'); ?>
+
+        <?php include_once('main-websit-eresource/about.php'); ?>
+
+        <?php include_once('main-websit-eresource/service.php'); ?>
+
+        <?php include_once('main-websit-eresource/login.php'); ?>
+       
+        <?php include_once('main-websit-eresource/register.php'); ?>
+
+        <?php include_once('main-websit-eresource/contact.php'); ?>
+
+
+    </dl>
+    
+                <div class="para"></div>
+        <?php include_once('main-websit-eresource/footer.php') ?>
+</div>
+</body>
+</html>
